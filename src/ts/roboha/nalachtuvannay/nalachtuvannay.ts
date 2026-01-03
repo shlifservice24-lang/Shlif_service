77b45bc5cb78199781b0fab07df77d1919f33f4c
shlifservice24-lang <shlifservice24-lang.@gmail.com>
2026-01-03 09:13:21 +0200

    deploy: 2026-01-03 09:13:21

--- src/ts/roboha/nalachtuvannay/nalachtuvannay.ts
+++ src/ts/roboha/nalachtuvannay/nalachtuvannay.ts
@@ -6,7 +6,7 @@ const SETTINGS = {
   1: { id: "toggle-shop", label: "ПІБ _ Магазин", class: "_shop" },
   2: { id: "toggle-receiver", label: "Каталог", class: "_receiver" },
   3: { id: "toggle-zarplata", label: "Зарплата", class: "_zarplata" },
-  4: { id: "percentage-value", label: "Націна на запчастини", class: "_percentage" },
+  4: { id: "percentage-value", label: "Націнка на запчастини", class: "_percentage" },
 /*   5: { id: "toggle-sms", label: "SMS", class: "_sms" }, */
 };
 
