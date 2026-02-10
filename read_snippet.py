c670bfbc9c97382d5187735c1b999fce3c257bf8
shlifservice24-lang <shlifservice24@gmail.com>
2026-02-10 15:55:53 +0200

    deploy: 2026-02-10 15:55

---
+++ read_snippet.py
@@ -0,0 +1,7 @@
+
+try:
+    with open(r'd:\Alim\Проект\Shlif_service\Shlif_service\src\ts\roboha\tablucya\tablucya.ts', 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        print(''.join(lines[837:930]))
+except Exception as e:
+    print(e)
